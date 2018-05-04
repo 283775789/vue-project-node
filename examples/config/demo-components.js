dbf62a9a0368ef8913f0ef4692bf27554837dc9f
rew3c <rew3c@qq.com>
2018-05-04 18:12:35 +0800

    '新提交'

--- examples/config/demo-components.js
+++ examples/config/demo-components.js
@@ -1,9 +1,13 @@
 // 注册通用的全局组件
 import Vue from 'vue'
 import PoppaneBase from '@/examples/demo/poppane-base'
+import ButtonBase from '@/examples/demo/button-base'
+import DesignGuid from '@/examples/docs/design'
 
 const components = [
-  PoppaneBase
+  PoppaneBase,
+  ButtonBase,
+  DesignGuid
 ]
 
 components.forEach(function (component) {
--- handler/markdown.js
+++ handler/markdown.js
@@ -12,8 +12,12 @@ module.exports = function (data) {
     if (demoReg.test(dataRow[i])) {
       const matches = demoReg.exec(dataRow[i])
 
-      const code = fs.readFileSync(`examples/demo/${matches[1].replace('tw-', '')}.vue`, 'utf-8').replace(/template\s+name=".*"/, 'template')
-      dataRow[i] = `<div class="tw-dropdown-demo"><${matches[1]} /></div>\n<span class="tw-markdown-shortcut"><span>快捷键 | </span>${matches[1]}</span>\n\`\`\`xml\n${code}\`\`\``
+      try {
+        const code = fs.readFileSync(`examples/demo/${matches[1].replace('tw-', '')}.vue`, 'utf-8').replace(/template\s+name=".*"/, 'template')
+        dataRow[i] = `<div class="tw-dropdown-demo"><${matches[1]} /></div>\n<span class="tw-markdown-shortcut"><span>快捷键 | </span>${matches[1]}</span>\n\`\`\`xml\n${code}\`\`\``
+      } catch (e) {
+        dataRow[i] = `<div class="tw-dropdown-demo"><${matches[1]} /></div>\n`
+      }
     }
   }
 
